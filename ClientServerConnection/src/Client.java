88eb2c2338580cd13f1440ef91deffb9341ae0d4
mohamedand4b <120221149+mohamedand4b@users.noreply.github.com>
2022-12-30 18:42:28 +0200

    Add files via upload

---
+++ ClientServerConnection/src/Client.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket s = new Socket ("localhost",4999); 
+
+	}
+
+}
